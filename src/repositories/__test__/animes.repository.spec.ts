19fd7d0f8311d3f496b28fd4105741094df319cf
arlops22 <ary.s.lopes22@gamil.com>
2026-08-09 16:25:42 -0300

    test: repository unit test

--- src/repositories/__test__/animes.repository.spec.ts
+++ src/repositories/__test__/animes.repository.spec.ts
@@ -1,25 +1,37 @@
 import { it, describe, expect } from '@jest/globals';
 
-import { Anime } from '../../interfaces/anime.interface';
-import { Prisma } from '../../../generated/prisma/client';
 import prismaMock from '../../__mocks__/prisma-client.mock';
 import AnimesRepositoryPrisma from '../animes.repository';
+import { Anime, AnimeCreatePayload, AnimeUpdatePayload } from '../../interfaces/anime.interface';
 
 describe('Animes Repository', () => {
     const animesRepository = new AnimesRepositoryPrisma(prismaMock);
+    const mockAnime: Anime = {
+        id: 1,
+        name: 'Anime Test',
+        summary: null,
+        author: null,
+        thumbnail: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    };
 
-    describe('Method findAll', () => {
-        it('should return all animes correctly', async () => {
-            const mockAnime: Anime = {
-                id: 1,
+    describe('Method create', () => {
+        it('should create an anime', async () => {
+            const mockAnimePayload: AnimeCreatePayload = {
                 name: 'Anime Test',
-                summary: 'This is a test summary',
-                author: null,
-                thumbnail: null,
-                createdAt: new Date(),
-                updatedAt: new Date(),
             };
 
+            prismaMock.anime.create.mockResolvedValue(mockAnime);
+
+            const anime = await animesRepository.create(mockAnimePayload);
+            expect(anime).toStrictEqual(mockAnime);
+            expect(prismaMock.anime.create).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('Method findAll', () => {
+        it('should return all animes', async () => {
             prismaMock.anime.findMany.mockResolvedValue([mockAnime]);
 
             const animes = await animesRepository.findAll();
@@ -29,27 +41,53 @@ describe('Animes Repository', () => {
         });
     });
 
-    describe('Method create', () => {
-        it('should create an anime correctly', async () => {
-            const mockAnimePayload: Prisma.AnimeCreateInput = {
+    describe('Method findById', () => {
+        it('should return a single anime', async () => {
+            prismaMock.anime.findUnique.mockResolvedValue(mockAnime);
+
+            const anime = await animesRepository.findById(1);
+
+            expect(anime).toStrictEqual(mockAnime);
+            expect(prismaMock.anime.findUnique).toHaveBeenCalledWith({
+                where: { id: 1 },
+            });
+        });
+    });
+
+    describe('Method update', () => {
+        it('should update an anime', async () => {
+            const mockAnimePayload: AnimeUpdatePayload = {
                 name: 'Anime Test',
+                author: 'Author test',
+                summary: 'This is a test summary',
             };
 
-            const createdAnime: Anime = {
-                ...mockAnimePayload,
-                id: 1,
-                author: null,
-                summary: null,
-                thumbnail: null,
-                createdAt: new Date(),
-                updatedAt: new Date(),
+            const updatedMockAnime = {
+                ...mockAnime,
+                author: 'Author test',
+                summary: 'This is a test summary',
             };
 
-            prismaMock.anime.create.mockResolvedValue(createdAnime);
+            prismaMock.anime.update.mockResolvedValue(updatedMockAnime);
 
-            const anime = await animesRepository.create(mockAnimePayload);
-            expect(anime).toStrictEqual(createdAnime);
-            expect(prismaMock.anime.create).toHaveBeenCalledTimes(1);
+            const anime = await animesRepository.update(1, mockAnimePayload);
+            expect(anime).toStrictEqual(updatedMockAnime);
+            expect(prismaMock.anime.update).toHaveBeenCalledWith({
+                where: { id: 1 },
+                data: mockAnimePayload,
+            });
+        });
+    });
+
+    describe('Method delete', () => {
+        it('should delete an anime', async () => {
+            prismaMock.anime.delete.mockResolvedValue(mockAnime);
+            const anime = await animesRepository.delete(1);
+
+            expect(anime).toStrictEqual(mockAnime);
+            expect(prismaMock.anime.delete).toHaveBeenCalledWith({
+                where: { id: 1 },
+            });
         });
     });
 });
